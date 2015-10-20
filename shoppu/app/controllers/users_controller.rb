ee0e9ed7af319aa1543ff87f935ac247916966e8
Mikey Y <drowningfish12@gmail.com>
2015-10-20 01:17:03 -0700

    finsihed the basic template for the sign up form and creating new users

--- shoppu/app/controllers/users_controller.rb
+++ shoppu/app/controllers/users_controller.rb
@@ -10,5 +10,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+   @user = User.new(user_params)
+
+
+   if @user.save
+     redirect_to @user
+   else
+     render 'new'
+   end
+ end
+
+  def user_params
+    params.require(:user).permit(:username, :address, :email,:password,:birthdate,
+    :first_name, :last_name, :is_mod, :rating => "0", :failed_login_attempts => "0")
+  end
+
 
 end
