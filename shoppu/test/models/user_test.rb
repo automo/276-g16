aa13785b24d32a5febdaa5dd424fe6037488ccb6
Nico <mca117@sfu.ca>
2015-10-21 21:30:48 -0700

    no message

--- shoppu/test/models/user_test.rb
+++ shoppu/test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(username: "Example User", email: "user@test.com",address:"sfu", is_mod:"true",first_name:"asd",last_name:"sada",password:"123456",password_confirmation:"123456",birthdate:"2012-11-11")
+    @user = User.new(username: "Example User", email: "user@example.com",address:"sfu", is_mod:"true",first_name:"asd",last_name:"sada",password:"123456",password_confirmation:"123456",birthdate:"2012-11-11")
   end
 
   test "user should be valid" do
@@ -25,22 +25,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "should accept valid mail format" do
-  valid_mailadresses = %w[user@test.com USER@test.com test-ds_fs@test.test.org
+  test "accept valid mail format" do
+  va_add = %w[user@test.com USER@test.com test-ds_fs@test.dsf.org
                        f.l@test.com sd2vf+nico@test.cn]
-  valid_mailadresses.each do |valid_mailadresse|
-    @user.email = valid_mailadresse
-    assert @user.valid?, "#{valid_mailadresse.inspect} should be valid"
+  va_add.each do |valid_mailadd|
+    @user.email = valid_mailadd
+    assert @user.valid?, "#{valid_mailadd.inspect} should be valid"
   end
 end
 
 
-  test "should reject invalid email format" do
-  invalid_mailadresses = %w[user@test,de user_test_test.de user.test@test.
-                        user@test_test.de user@test+test.de]
-  invalid_mailadresses.each do |invalid_mailadresse|
-    @user.email = invalid_mailadresse
-    assert_not @user.valid?, "#{invalid_mailadresse} should be invalid"
+  test "reject invalid email format" do
+  inva_add = %w[user@test,sd user_test_test.csf user.test@test.
+                        user@test_test.dom user@test+test.dwqd]
+  inva_add.each do |invalid_mailadd|
+    @user.email = invalid_mailadd
+    assert_not @user.valid?, "#{invalid_mailadd} should be invalid"
   end
 end
 
