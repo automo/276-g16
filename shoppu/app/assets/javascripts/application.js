58712335b6afa66d2570b9712011b383cd94fb6a
Mikey Y <drowninfish12@gmail.com>
2015-11-04 21:48:45 -0800

    Changing the front page of the shoppu adding the new about info and also menu bar on the top right corner known bugs   the menu is occupying the blank space on top. might need some help with it the log in and log out work and sign up also work might need to change the font size and colour   and may need different image as background

--- shoppu/app/assets/javascripts/application.js
+++ shoppu/app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// Test adding boostrap.min
+//
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
