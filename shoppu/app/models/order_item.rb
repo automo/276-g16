b08c3c873f054b58f002bd53af5d20f5538a6222
Karan Sharma <ksharma0017@gmail.com>
2015-11-08 04:48:09 -0800

    Add independent styles (Read Full Commit Message!)
    - Running 'rake db:drop db:migrate' may be needed
    - Order_request form is accessible at http://localhost:3000/order_requests
    - Note: In Atom, right-click on the shoppu directory and search
    -       in the directory for '~K' (without single quotes)
    -       for undo-ing the independent style I applied for order_requests.
    - Reference for this branch: https://github.com/mackenziechild/Todo-App

--- shoppu/app/models/order_item.rb
+++ shoppu/app/models/order_item.rb
@@ -1,3 +1,7 @@
 class OrderItem < ActiveRecord::Base
-  belongs_to :order
+  belongs_to :order_request
+
+  def completed?
+    !completed_at.blank?
+  end
 end
--- shoppu/db/migrate/20151108082355_create_order_items.rb
+++ shoppu/db/migrate/20151108082355_create_order_items.rb
@@ -1,9 +1,9 @@
 class CreateOrderItems < ActiveRecord::Migration
   def change
     create_table :order_items do |t|
-      t.string :status, :null=>false
+      t.string :status, :default=>"open"
       t.text :content
-      t.references :order, :null=>false, index: true, foreign_key: true
+      t.references :order_request, :null=>false, index: true, foreign_key: true
 
       t.timestamps null: false
     end
