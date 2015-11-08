4a7c7a158282815ae2e97afd1da4febb82cbbfea
Karan Sharma <ksharma0017@gmail.com>
2015-11-08 11:20:58 -0800

    Add null constraint on order item

--- shoppu/db/migrate/20151108082355_create_order_items.rb
+++ shoppu/db/migrate/20151108082355_create_order_items.rb
@@ -1,8 +1,8 @@
 class CreateOrderItems < ActiveRecord::Migration
   def change
     create_table :order_items do |t|
-      t.string :status, :default=>"open"
-      t.text :content
+      t.string :status, :default=>"open", :null=>false
+      t.text :content, :null=>false
       t.references :order_request, :null=>false, index: true, foreign_key: true
 
       t.timestamps null: false
