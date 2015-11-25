7fc6b8c5634380ab45ca5a12b7fe0f9a517a27d0
Mikey Y <drowninfish12@gmail.com>
2015-11-24 22:44:12 -0800

    siomple google map view as image need more modification

--- shoppu/db/schema.rb
+++ shoppu/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119075941) do
+ActiveRecord::Schema.define(version: 20151125055625) do
 
   create_table "order_items", force: :cascade do |t|
     t.string   "status",           default: "open"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 20151119075941) do
     t.string   "remember_digest"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
