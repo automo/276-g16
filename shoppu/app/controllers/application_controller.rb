0c9fdc9a4fcbf426c19f15bf991280a931e02042
Karan Sharma <ksharma0017@gmail.com>
2015-11-27 02:37:29 -0800

    Return users to last page before redirection

--- shoppu/app/controllers/application_controller.rb
+++ shoppu/app/controllers/application_controller.rb
@@ -18,8 +18,8 @@ class ApplicationController < ActionController::Base
   # Confirms a logged-in user.
   def logged_in_user
     unless logged_in?
-      # store_location
       flash[:danger] = "Please log in."
+      store_location
       redirect_to login_url
     end
   end
--- shoppu/app/controllers/sessions_controller.rb
+++ shoppu/app/controllers/sessions_controller.rb
@@ -7,10 +7,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
-
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-     redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -20,10 +19,5 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
-  end
-
-  # Stores the URL trying to be accessed.
-  def store_location
-    session[:forwarding_url] = request.url if request.get?
   end
 end
