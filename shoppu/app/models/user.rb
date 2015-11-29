5475b09690a0f376717419404c6977ce752dc614
Karan Sharma <ksharma0017@gmail.com>
2015-11-29 02:21:38 -0800

    Clean up code

--- shoppu/app/models/user.rb
+++ shoppu/app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  # has_many :orders
   has_many :owned_orders, foreign_key: 'owner_id', class_name: "OrderRequest", dependent: :destroy
   has_many :serviced_orders, foreign_key: 'servicer_id', class_name: "OrderRequest", dependent: :destroy
 
@@ -10,10 +9,12 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: {case_sensitive: false},
             length: {maximum:50}
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
   validates :first_name, presence: true
@@ -25,8 +26,6 @@ class User < ActiveRecord::Base
 
   validates :age, numericality: {greater_than:0}
   validates :birthdate, presence: true
-
-
 
   def age
     now = Time.now.utc.to_date
@@ -69,11 +68,11 @@ class User < ActiveRecord::Base
   end
 
   # Sets the password reset attributes.
-    def create_reset_digest
-      self.reset_token = User.new_token
-      update_attribute(:reset_digest,  User.digest(reset_token))
-      update_attribute(:reset_sent_at, Time.zone.now)
-    end
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
 
   # Sends password reset email.
   def send_password_reset_email
@@ -83,5 +82,4 @@ class User < ActiveRecord::Base
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
-
 end
