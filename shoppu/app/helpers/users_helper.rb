8faf615bc1137f3db3537a85642b10d2ea7bdf10
Karan Sharma <ksharma0017@gmail.com>
2015-11-29 19:52:57 -0800

    Fix helper Boolean conditions

--- shoppu/app/helpers/users_helper.rb
+++ shoppu/app/helpers/users_helper.rb
@@ -1,21 +1,25 @@
 module UsersHelper
   def getOpenCount(user_id)
-    order_requests = User.find(user_id).owned_orders.all
-    return order_requests.select{|order_request| order_request.status != "open"}.size
+    order_requests = User.find_by_id(user_id).owned_orders.all
+    return -1 if order_requests.blank?
+    return order_requests.select{|order_request| order_request.status == "open"}.size
   end
 
   def getAcceptedCount(user_id)
-    order_requests = User.find(user_id).owned_orders.all
-    return order_requests.select{|order_request| order_request.status != "accepted"}.size
+    order_requests = User.find_by_id(user_id).owned_orders.all
+    return -1 if order_requests.blank?
+    return order_requests.select{|order_request| order_request.status == "accepted"}.size
   end
 
   def getCompletedCount(user_id)
-    order_requests = User.find(user_id).owned_orders.all
-    return order_requests.select{|order_request| order_request.status != "completed"}.size
+    order_requests = User.find_by_id(user_id).owned_orders.all
+    return -1 if order_requests.blank?
+    return order_requests.select{|order_request| order_request.status == "completed"}.size
   end
 
   def getHiddenCount(user_id)
-    order_requests = User.find(user_id).owned_orders.all
-    return order_requests.select{|order_request| order_request.status != "hidden"}.size
+    order_requests = User.find_by_id(user_id).owned_orders.all
+    return -1 if order_requests.blank?
+    return order_requests.select{|order_request| order_request.status == "hidden"}.size
   end
 end
