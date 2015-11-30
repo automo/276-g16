722e3bc77e93312cfe1bf9bf571e6f3666760aeb
Karan Sharma <ksharma0017@gmail.com>
2015-11-30 00:16:30 -0800

    Refactor and add more helper methods for User Profile

--- shoppu/app/helpers/users_helper.rb
+++ shoppu/app/helpers/users_helper.rb
@@ -1,25 +1,43 @@
 module UsersHelper
-  def getOpenCount(user_id)
+  # Note: recognized status_types are "accepted", "hidden", "open", "completed"
+  def count_orders_by_status(user_id, status_type)
     order_requests = User.find_by_id(user_id).owned_orders.all
     return -1 if order_requests.blank?
-    return order_requests.select{|order_request| order_request.status == "open"}.size
+    return order_requests.select{|order_request| order_request.status == status_type}.size
   end
 
-  def getAcceptedCount(user_id)
-    order_requests = User.find_by_id(user_id).owned_orders.all
+  # Note: recognized status_types are "accepted", "hidden", "open", "completed"
+  def count_all_orders_by_status(status_type)
+    order_requests = OrderRequest.all
     return -1 if order_requests.blank?
-    return order_requests.select{|order_request| order_request.status == "accepted"}.size
+    return order_requests.select{|order_request| order_request.status == status_type}.size
   end
 
-  def getCompletedCount(user_id)
-    order_requests = User.find_by_id(user_id).owned_orders.all
-    return -1 if order_requests.blank?
-    return order_requests.select{|order_request| order_request.status == "completed"}.size
+  def count_all_orders
+    return OrderRequest.all.size
   end
-
-  def getHiddenCount(user_id)
-    order_requests = User.find_by_id(user_id).owned_orders.all
-    return -1 if order_requests.blank?
-    return order_requests.select{|order_request| order_request.status == "hidden"}.size
-  end
+  #
+  # def getOpenCount(user_id)
+  #   order_requests = User.find_by_id(user_id).owned_orders.all
+  #   return -1 if order_requests.blank?
+  #   return order_requests.select{|order_request| order_request.status == "open"}.size
+  # end
+  #
+  # def getAcceptedCount(user_id)
+  #   order_requests = User.find_by_id(user_id).owned_orders.all
+  #   return -1 if order_requests.blank?
+  #   return order_requests.select{|order_request| order_request.status == "accepted"}.size
+  # end
+  #
+  # def getCompletedCount(user_id)
+  #   order_requests = User.find_by_id(user_id).owned_orders.all
+  #   return -1 if order_requests.blank?
+  #   return order_requests.select{|order_request| order_request.status == "completed"}.size
+  # end
+  #
+  # def getHiddenCount(user_id)
+  #   order_requests = User.find_by_id(user_id).owned_orders.all
+  #   return -1 if order_requests.blank?
+  #   return order_requests.select{|order_request| order_request.status == "hidden"}.size
+  # end
 end
