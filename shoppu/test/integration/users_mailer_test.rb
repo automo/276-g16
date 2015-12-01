5fd24767fe6229057a7ac2ab0809b4dd08c5640b
Karan Sharma <ksharma0017@gmail.com>
2015-12-01 00:28:13 -0800

    Fix/Add integration tests

--- shoppu/test/integration/users_mailer_test.rb
+++ shoppu/test/integration/users_mailer_test.rb
@@ -4,10 +4,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   def setup
     ActionMailer::Base.deliveries.clear
-    # @user = users(:UserTwo)
   end
 
-  test "invalid signup information" do
+  test "no mail sent after invalid signup" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: {
@@ -20,10 +19,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password:              "foo",
                                password_confirmation: "bar" }
     end
-    assert_template 'users/new'
+    assert_template partial: 'shared/_error_messages'
+    assert_template 'users/new', layout: 'layouts/application'
+    assert_template 'layouts/_head'
+    assert_template 'layouts/_body'
+    assert_template 'layouts/_footer'
   end
 
-  test "valid signup information" do
+  test "send mail after valid signup" do
     get signup_path
     post users_path, user: { username: "new_user",
                              email: "new_user@domain.com",
@@ -34,7 +37,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                              birthdate: "1973-11-11",
                              password: "password123",
                              password_confirmation: "password123" }
-    follow_redirect!
-    assert_template 'users/show'
+    assert_template 'user_mailer/user_email', layout: 'layouts/mailer'
+    # assert_template layout: 'layouts/mailer'
+
+    # assert_template partial: 'user_mailer/user_email'
   end
 end
