ad0c72fed3c8675f418ee7616f17d7e03d98bc54
Agnes <utomo.agnescarissa@gmail.com>
2015-12-01 10:36:16 -0800

    order_items tests

--- shoppu/test/controllers/order_items_controller_test.rb
+++ shoppu/test/controllers/order_items_controller_test.rb
@@ -4,8 +4,28 @@ class OrderItemsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @user = users(:UserTwo)
+    log_in_as(@user)
+    @order_request = @user.owned_orders.first
+    @order_item = @order_request.order_items.first
+  end
+
+  test "should create order_item" do
+    assert_difference('OrderItem.count') do
+      post :create, article: {content: 'Hi'}
+    end
+    assert_redirected_to order_request_path
+    assert_equal 'Order item was added.', flash[:success]
+  end
+
+  test "should destroy order_item" do
+    assert_difference('OrderItem.count',-1) do
+      delete :destroy, id: @order_item.id
+    end
+    assert_redirected_to order_request_path
+    assert_equal 'Order item was deleted.', flash[:success]
+  end
 
   
-
-
 end
