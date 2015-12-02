c417a829a928fdccecd443483523fba6d3e827b8
Karan Sharma <ksharma0017@gmail.com>
2015-12-01 18:47:07 -0800

    Fix controller tests and navbar link

--- shoppu/test/controllers/order_items_controller_test.rb
+++ shoppu/test/controllers/order_items_controller_test.rb
@@ -2,25 +2,25 @@ require 'test_helper'
 
 class OrderItemsControllerTest < ActionController::TestCase
   setup do
-    @user = users(:UserTwo)
-    log_in_as(@user)
     @order_item = OrderItem.first
-    @order_request = OrderItem.first.order_request
+    @order_request = @order_item.order_request
+    @user = @order_request.owner
+    log_in_as(@user)
   end
 
   test "should create order_item" do
     assert_difference('OrderItem.count') do
-      post :create, order_item: {content: 'Hi'}
+      post :create, order_item: {content: 'Hi'}, order_request_id: @order_request.id
     end
-    assert_redirected_to order_request_path
+    assert_redirected_to @order_request
     assert_equal 'Order item was added.', flash[:success]
   end
 
   test "should destroy order_item" do
     assert_difference('OrderItem.count', -1) do
-      delete :destroy, id: @order_item
+      delete :destroy, id: @order_item, order_request_id: @order_request.id
     end
-    assert_redirected_to order_request_path
+    assert_redirected_to @order_request
     assert_equal 'Order item was deleted.', flash[:success]
   end
 
